3f7009c5ead9fffe6fc3f78bf148fec1f826b191
Sean Wesenberg <wookets@gmail.com>
2018-02-14 20:43:27 -0600

    Initial Commit

---
+++ todo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  todo
+//
+//  Created by Sean Wesenberg on 2/14/18.
+//  Copyright © 2018 Sean Wesenberg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
