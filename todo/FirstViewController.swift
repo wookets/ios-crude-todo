a80381034b33041d56d233a7c9c78993636c1911
Sean Wesenberg <wookets@gmail.com>
2018-02-14 21:22:28 -0600

    Initial commit of CVP (crude viable product)

--- todo/FirstViewController.swift
+++ todo/FirstViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var list = ["Buy Milk", "Run 5 Miles", "Get Peter", "Plant new plants"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- todo/SecondViewController.swift
+++ todo/SecondViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: Any) {
+        if (input.text != "") {
+            list.append(input.text!)
+            input.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
